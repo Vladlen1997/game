4883b93d2720aff92ab5269b5123d0ba8b2e465f
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-04-20 11:46:22 +0300

    fixed a bug with the right key and added moving when pressed

--- game/scenes/mainScene.js
+++ game/scenes/mainScene.js
@@ -8,29 +8,33 @@ export default class mainScene extends Phaser.Scene {
     }
 
     preload(){
-        this.load.image('sky', '../game/assets/sky.png')
-        this.load.image('ground', '../game/assets/ground.png')
-        this.load.image('platform', '../game/assets/platform.png')
-        this.load.spritesheet('bishop', '../game/assets/Bishop/bishop.png', {frameWidth: 32, frameHeight: 32})
+        this.load.image('sky', '../game/assets/sky.png');
+        this.load.image('ground', '../game/assets/ground.png');
+        this.load.image('platform', '../game/assets/platform.png');
+        this.load.spritesheet('bishop', '../game/assets/Bishop/bishop.png', {frameWidth: 32, frameHeight: 32});
     }
 
     create(){
-        this.add.image(400, 300, 'sky')
-        this.ground = this.physics.add.staticGroup()
-        this.ground.create(400, 600, 'ground')
-        this.platforms = this.physics.add.staticGroup()
-        this.platforms.create(200, 350, 'platform')
-        this.platforms.create(50, 500, 'platform')
-        this.platforms.create(550, 350, 'platform')
-        this.platforms.create(600, 500, 'platform')
-        this.bishop = this.physics.add.sprite(0, 0, 'bishop')
-        this.bishop.setCollideWorldBounds(true)
-        this.bishop.setBounce(0.2)
+        this.add.image(400, 300, 'sky');
 
-        this.cursor = this.input.keyBoard.createCursorKeys();
+        this.ground = this.physics.add.staticGroup();
+        this.ground.create(400, 600, 'ground');
 
-        this.physics.add.collider(this.bishop, this.platforms)
-        this.physics.add.collider(this.bishop, this.ground)
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(200, 350, 'platform');
+
+        this.platforms.create(50, 500, 'platform');
+        this.platforms.create(550, 350, 'platform');
+        this.platforms.create(600, 500, 'platform');
+
+        this.bishop = this.physics.add.sprite(100, 100, 'bishop');
+        this.bishop.setCollideWorldBounds(true);
+        this.bishop.setBounce(0.2);
+
+        this.cursor = this.input.keyboard.createCursorKeys();
+
+        this.physics.add.collider(this.bishop, this.platforms);
+        this.physics.add.collider(this.bishop, this.ground);
 
     }
 
@@ -38,9 +42,13 @@ export default class mainScene extends Phaser.Scene {
         if(this.cursor.left.isDown){
             this.bishop.setVelocityX(-160)
         } else if(this.cursor.right.isDown){
-            this.bishop.setvelocityY(160)
+            this.bishop.setVelocityX(160)
         } else {
-            this.player.setVelocityX(0)
+            this.bishop.setVelocityX(0)
+        }
+
+        if(this.cursor.up.isDown && this.bishop.body.touching.down) {
+            this.bishop.setVelocityY(-330)
         }
     }
 
