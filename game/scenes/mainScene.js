7198d259a5ffb4ea9bb1d0cb9c345b95f91a6e62
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-04-20 10:34:46 +0300

    I put the control keys and indicate the action on each key

--- game/scenes/mainScene.js
+++ game/scenes/mainScene.js
@@ -4,6 +4,7 @@ export default class mainScene extends Phaser.Scene {
         this.ground;
         this.platforms;
         this.bishop;
+        this.cursor;
     }
 
     preload(){
@@ -25,10 +26,22 @@ export default class mainScene extends Phaser.Scene {
         this.bishop = this.physics.add.sprite(0, 0, 'bishop')
         this.bishop.setCollideWorldBounds(true)
         this.bishop.setBounce(0.2)
+
+        this.cursor = this.input.keyBoard.createCursorKeys();
+
         this.physics.add.collider(this.bishop, this.platforms)
         this.physics.add.collider(this.bishop, this.ground)
+
     }
 
-    update(){}
+    update(){
+        if(this.cursor.left.isDown){
+            this.bishop.setVelocityX(-160)
+        } else if(this.cursor.right.isDown){
+            this.bishop.setvelocityY(160)
+        } else {
+            this.player.setVelocityX(0)
+        }
+    }
 
 }
